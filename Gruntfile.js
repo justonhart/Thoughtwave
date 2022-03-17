f86cf5ba3739aa64d3a7ac13949d8196e0ef02f8
Juston Hart <juston@ou.edu>
2022-03-17 13:15:09 -0500

    Initialize repo

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+module.exports = function(grunt) {
+    
+    grunt.loadNpmTasks('grunt-screeps');
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: process.env.email,
+                token: process.env.token,
+                branch: 'default',
+            },
+            dist: {
+                src: ['dist/*.js']
+            }
+        }
+    });
+}
