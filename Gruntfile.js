1a663e6a6714f03bafdc48a0b39d81958e597dd7
Melvin <melvin.lipka@gmx.de>
2022-03-17 21:23:18 +0100

    PreCommit Formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,18 @@
-require('dotenv').config()
+require('dotenv').config();
 
-module.exports = function(grunt) {
-    
-    grunt.loadNpmTasks('grunt-screeps');
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-screeps');
 
-    grunt.initConfig({
-        screeps: {
-            options: {
-                email: process.env.email,
-                token: process.env.token,
-                branch: 'default',
-            },
-            dist: {
-                src: ['dist/*.js']
-            }
-        }
-    });
-}
+  grunt.initConfig({
+    screeps: {
+      options: {
+        email: process.env.email,
+        token: process.env.token,
+        branch: 'default',
+      },
+      dist: {
+        src: ['dist/*.js'],
+      },
+    },
+  });
+};
--- README.md
+++ README.md
@@ -4,12 +4,14 @@ A next-generation AI for Screeps
 
 ## NPM packages
 
-* Grunt + [grunt-screeps](https://github.com/screeps/grunt-screeps) - for deployment
-* dotenv - for isolating login values from GH
-* typescript
+- Grunt + [grunt-screeps](https://github.com/screeps/grunt-screeps) - for deployment
+- dotenv - for isolating login values from GH
+- typescript
+- husky + prettier - uniform formatting
 
 ## Installation
 
-* Install Grunt: npm install -g grunt-cli
-* Input Data from Screeps Account into "example.env" and rename it to ".env"
-* Upload to Screeps: grunt screeps
+- Install Grunt: npm install -g grunt-cli
+- Input Data from Screeps Account into "example.env" and rename it to ".env"
+- Run in Project: npm install
+- Upload to Screeps: grunt screeps
