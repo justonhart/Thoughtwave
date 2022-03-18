b7e41a6dead2d2e77863df51ee662fb673cd794b
Juston Hart <contact@justonh.art>
2022-03-18 11:43:20 -0500

    Remove subfolders from imports
    Grunt flattens the dist folder upon deploy, so the requires needed to be
    modified to reflect that

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-screeps');
-
+  grunt.loadNpmTasks('grunt-replace');
   grunt.initConfig({
     screeps: {
       options: {
@@ -21,5 +21,26 @@ module.exports = function (grunt) {
         ],
       },
     },
+    replace: {
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: /(?<=require\(".)(.*)(?=\/[\w]+")/g,
+              replacement: ''
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'dist/',
+            src: ['**/*.js'],
+            dest: 'dist/'
+          }
+        ]
+      }
+    }
   });
 };
