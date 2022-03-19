55de12be98163052c38f52abe4ed45fb5c4e22f8
Melvin <melvin.lipka@gmx.de>
2022-03-19 19:47:31 +0100

    [ADD] Initial Pathing Prototype

--- src/types/creep.ts
+++ src/types/creep.ts
@@ -1,7 +1,13 @@
 interface CreepMemory {
   room?: string;
   role: Role;
-  _move?: Partial<TravelData>;
+  _move?: TravelData;
+  prevCoords?: Coord;
+  stuckCount?: number;
+}
+
+interface Creep {
+  travelTo(destination: HasPos | RoomPosition, opts?: TravelToOpts): CreepMoveReturnCode | ERR_NO_PATH | ERR_INVALID_TARGET | ERR_NOT_FOUND;
 }
 
 const enum Role {
