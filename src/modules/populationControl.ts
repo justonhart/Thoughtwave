53e64e46065d09e601b720d5e076609cf18a152f
Juston Hart <contact@justonh.art>
2022-03-20 14:14:40 -0500

    Attempt to add dynamic spawn size

--- src/modules/populationControl.ts
+++ src/modules/populationControl.ts
@@ -16,12 +16,35 @@ export default function populationControl(spawn: StructureSpawn) {
 
     if (roomCreeps.filter((creep) => creep.memory.role === Role.WORKER).length < WORKER_LIMIT) {
         options.memory.role = Role.WORKER;
-        spawn.spawnCreep([WORK, CARRY, MOVE], `${options.memory.role} ${Game.time}`, options);
     } else if (roomCreeps.filter((creep) => creep.memory.role === Role.UPGRADER).length < UPGRADER_LIMIT) {
         options.memory.role = Role.UPGRADER;
-        spawn.spawnCreep([WORK, CARRY, MOVE], `${options.memory.role} ${Game.time}`, options);
     } else if (roomCreeps.filter((creep) => creep.memory.role === Role.MAINTAINTER).length < MAINTAINTER_LIMIT) {
         options.memory.role = Role.MAINTAINTER;
-        spawn.spawnCreep([WORK, CARRY, MOVE], `${options.memory.role} ${Game.time}`, options);
+    }
+
+    if (options.memory.role) {
+        let partsArray: BodyPartConstant[] = [];
+        let partsBlock = [WORK, CARRY, MOVE];
+
+        for (let i = 0; i < Math.floor(spawn.room.energyCapacityAvailable / 200); i++) {
+            partsArray = partsArray.concat(partsBlock);
+        }
+
+        let result = spawn.spawnCreep(partsArray, `${options.memory.role} ${Game.time}`, options);
+
+        //if there are no harvesters, and there is not enough energy to spawn one
+        if (result === ERR_NOT_ENOUGH_ENERGY && options.memory.role === Role.WORKER) {
+            let potentialWorkers = roomCreeps.filter((creep) => creep.memory.role !== Role.WORKER && creep.getActiveBodyparts(WORK));
+            if (potentialWorkers.length) {
+                let creepToConvert = potentialWorkers.sort((creep) => creep.getActiveBodyparts(WORK)).shift();
+                creepToConvert.memory.role = Role.WORKER;
+                creepToConvert.memory.targetId = null;
+            } else if (!_.filter(Game.creeps, (creep) => creep.memory.role === Role.WORKER && creep.memory.home === spawn.room.name).length) {
+                //spawn first available harvester
+                partsArray = [];
+                for (let i = 0; i < Math.floor(spawn.room.energyAvailable / 200); i++) partsArray = partsArray.concat(partsBlock);
+                spawn.spawnCreep(partsArray, `${options.memory.role} ${Game.time}`, options);
+            }
+        }
     }
 }
