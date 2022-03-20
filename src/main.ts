71cafb387a401ff786c162d6d750cd55e2e8035d
Juston Hart <contact@justonh.art>
2022-03-20 11:30:50 -0500

    Add try/catches to main to prevent CATASTROPHE

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Console } from 'console';
 import driveCreep from './modules/creepDriver';
 import { manageMemory } from './modules/memoryManagement';
 import populationControl from './modules/populationControl';
@@ -7,19 +8,35 @@ require('./prototypes/requirePrototypes');
 module.exports.loop = function () {
     Object.values(Game.spawns).forEach((spawn) => {
         if (!spawn.spawning) {
-            populationControl(spawn);
+            try {
+                populationControl(spawn);
+            } catch (e) {
+                console.log(`Error caught in ${spawn.name}: \n${e}`);
+            }
         }
     });
 
     Object.values(Game.creeps).forEach((creep) => {
         if (!creep.spawning) {
-            driveCreep(creep);
+            try {
+                driveCreep(creep);
+            } catch (e) {
+                console.log(`Error caught in ${creep.name}: \n${e}`);
+            }
         }
     });
 
     Object.values(Game.rooms).forEach((room) => {
-        driveRoom(room);
+        try {
+            driveRoom(room);
+        } catch (e) {
+            console.log(`Error caught in ${room.name}: \n${e}`);
+        }
     });
 
-    manageMemory();
+    try {
+        manageMemory();
+    } catch (e) {
+        console.log(`Error caught in memory management: \n${e}`);
+    }
 };
