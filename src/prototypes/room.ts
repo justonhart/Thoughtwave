56a4d5ca1a5abea8cecb6959b6887ea3b64f6dd6
Juston Hart <contact@justonh.art>
2022-03-20 10:30:08 -0500

    Convert source access array to set to remove dupes

--- src/prototypes/room.ts
+++ src/prototypes/room.ts
@@ -1,10 +1,10 @@
 Room.prototype.initRoomMemory = function (this: Room) {
-    this.memory.availableSourceAccessPoints = this.find(FIND_SOURCES) //
+    this.memory.availableSourceAccessPoints = Array.from(new Set(this.find(FIND_SOURCES) //
         .flatMap((source) =>
             this.lookForAtArea(LOOK_TERRAIN, source.pos.y - 1, source.pos.x - 1, source.pos.y + 1, source.pos.x + 1, true)
                 .filter((terrain) => terrain.terrain != 'wall')
                 .map((terrain) => new RoomPosition(terrain.x, terrain.y, this.name).toMemSafe())
-        );
+        )));
 
     this.memory.sourceAccessPointCount = this.memory.availableSourceAccessPoints.length;
 };
