87b265b72091e5cdb541776bab3f0d8f973e5eeb
Juston Hart <contact@justonh.art>
2022-04-05 11:16:55 -0500

    Remove distribution responsibilities from Transporter
    Led to inefficiencies when collecting, need to reconsider implementation

--- src/roles/transporter.ts
+++ src/roles/transporter.ts
@@ -8,15 +8,6 @@ export class Transporter extends TransportCreep {
             target = this.findCollectionTarget();
         }
 
-        // //store resources before running refills
-        // if (!target && this.store.energy < this.store.getUsedCapacity()) {
-        //     target = this.room.storage.id;
-        // }
-
-        // if (!target && (this.room.storage.store.energy > 0 || this.store.energy > 0)) {
-        //     target = this.findRefillTarget();
-        // }
-
         if (!target) {
             target = this.homeroom.storage?.id;
         }
