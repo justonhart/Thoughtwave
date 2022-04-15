a2cdab6b3cfdde5b953b59f6bb2e9f8163407bdd
Juston Hart <contact@justonh.art>
2022-04-15 07:47:09 -0500

    Only mark room as claimed if it was actually claimed

--- src/roles/claimer.ts
+++ src/roles/claimer.ts
@@ -10,19 +10,21 @@ export class Claimer extends WaveCreep {
 
         // Go to the target room
         if (this.travelToRoom(this.memory.destination) === IN_ROOM) {
-            // Claim Controller in target room
-            switch (this.claimController(this.room.controller)) {
-                case ERR_NOT_IN_RANGE:
-                    this.travelTo(this.room.controller, { range: 1, swampCost: 1 });
-                    break;
-                case ERR_INVALID_TARGET:
-                case OK:
-                    console.log(`${this.room.name} has been claimed!`);
-
-                    let opIndex = Memory.empire.colonizationOperations.findIndex((op) => op.destination === this.room.name);
-                    Memory.empire.colonizationOperations[opIndex].stage = ColonizeStage.BUILD;
-                    this.suicide();
-                    break;
+            if (this.room.controller.my) {
+                console.log(`${this.room.name} has been claimed!`);
+                let opIndex = Memory.empire.colonizationOperations.findIndex((op) => op.destination === this.room.name);
+                Memory.empire.colonizationOperations[opIndex].stage = ColonizeStage.BUILD;
+                this.suicide();
+            } else {
+                // Claim Controller in target room
+                switch (this.claimController(this.room.controller)) {
+                    case ERR_NOT_IN_RANGE:
+                        this.travelTo(this.room.controller, { range: 1, swampCost: 1 });
+                        break;
+                    case ERR_INVALID_TARGET:
+                    case OK:
+                        break;
+                }
             }
         }
     }
