fa6ab498b07b2502484e5f63479d3617df4f8cab
Melvin Lipka <74310778+lipkam@users.noreply.github.com>
2022-05-29 17:05:25 +0200

    Claimer fix

--- src/roles/claimer.ts
+++ src/roles/claimer.ts
@@ -84,7 +84,7 @@ export class Claimer extends WaveCreep {
 
                     // Check if still claimed by enemy
                     const action =
-                        this.room.controller?.reservation?.username !== this.owner.username
+                        this.room.controller?.reservation?.username && this.room.controller?.reservation?.username !== this.owner.username
                             ? this.attackController(this.room.controller)
                             : this.claimController(this.room.controller);
                     // Claim Controller in target room
