7c900d006f38113bad35e66cb78b1b192b7445f4
Juston Hart <contact@justonh.art>
2022-06-20 13:53:06 -0500

    Optimize logic when upgraderLink exists

--- src/roles/upgrader.ts
+++ src/roles/upgrader.ts
@@ -2,17 +2,20 @@ import { WorkerCreep } from '../virtualCreeps/workerCreep';
 
 export class Upgrader extends WorkerCreep {
     protected run() {
-        if (!this.store.energy) {
-            if (!this.room.upgraderLink) {
+        if (!this.room.upgraderLink) {
+            if (!this.store.energy) {
                 this.gatherEnergy();
             } else {
+                this.runUpgradeJob();
+            }
+        } else {
+            if (this.store.energy <= this.getActiveBodyparts(WORK)) {
                 if (!this.pos.isNearTo(this.room.upgraderLink)) {
-                    this.travelTo(this.room.upgraderLink, { range: 1 });
+                    this.travelTo(this.room.upgraderLink);
                 } else {
                     this.withdraw(this.room.upgraderLink, RESOURCE_ENERGY);
                 }
             }
-        } else {
             this.runUpgradeJob();
         }
     }
