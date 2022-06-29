edc6d39f5b2677d3f48f1f1a14967967d1472dd1
Juston Hart <contact@justonh.art>
2022-06-29 15:00:15 -0500

    Restructure distributor findTarget to optimize collection

--- src/roles/distributor.ts
+++ src/roles/distributor.ts
@@ -4,10 +4,6 @@ export class Distributor extends TransportCreep {
     protected findTarget() {
         let target: any;
 
-        if (this.store.energy < this.store.getUsedCapacity() && this.room.storage?.store.getFreeCapacity()) {
-            target = this.homeroom.storage?.id;
-        }
-
         if (!target && this.homeroom.memory.labRequests?.length) {
             if (this.store.getUsedCapacity()) {
                 target = this.homeroom.storage.id;
@@ -18,10 +14,14 @@ export class Distributor extends TransportCreep {
         }
 
         if (!target && (this.homeroom.storage?.store.energy > 0 || this.store.energy > 0)) {
-            target = this.findRefillTarget();
+            if (this.store.energy < this.store.getUsedCapacity()) {
+                target = this.homeroom.storage.id;
+            } else {
+                target = this.findRefillTarget();
+            }
         }
 
-        if (!target && this.store.getUsedCapacity() < this.store.getCapacity() / 2) {
+        if (!target && this.store.getFreeCapacity()) {
             target = this.findCollectionTarget();
         }
 
