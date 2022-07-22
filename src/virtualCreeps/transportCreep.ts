421fb5ec9840c234540358792c9aae415cd12543
Juston Hart <contact@justonh.art>
2022-07-22 15:02:08 -0500

    Store resources before lab task

--- src/virtualCreeps/transportCreep.ts
+++ src/virtualCreeps/transportCreep.ts
@@ -297,6 +297,11 @@ export class TransportCreep extends WaveCreep {
                 !resourceList[req.resource] ? (resourceList[req.resource] = req.amount) : (resourceList[req.resource] += req.amount);
             });
 
+            if (Object.keys(this.store).some((res) => !resourceList[res])) {
+                this.storeCargo();
+                return;
+            }
+
             let totalAmountToGather = Math.min(
                 this.store.getCapacity(),
                 Object.values(resourceList).reduce((sum, next) => sum + next)
@@ -325,7 +330,7 @@ export class TransportCreep extends WaveCreep {
                 }
             }
         } else {
-            if (this.store.getUsedCapacity()) {
+            if (this.store.getUsedCapacity(this.memory.labRequests[0]?.resource)) {
                 let deliveryTarget = Game.getObjectById(requests[0].lab);
                 if (!this.pos.isNearTo(deliveryTarget)) {
                     this.travelTo(deliveryTarget, { range: 1 });
