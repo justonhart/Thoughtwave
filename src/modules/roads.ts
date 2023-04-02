91e66a4315fd0614133cce8c691a99fd42b1920a
Juston Hart <contact@justonh.art>
2023-04-02 13:01:44 -0400

    Prevent null reference in roadIsSafe

--- src/modules/roads.ts
+++ src/modules/roads.ts
@@ -244,7 +244,7 @@ export function roadIsSafe(roadKey: string) {
     return getAllRoadRooms(roadKey).every(
         (room) =>
             (Memory.roomData[room]?.hostile !== true && !Memory.remoteData[room]) ||
-            Memory.remoteData[room].threatLevel < RemoteRoomThreatLevel.ENEMY_ATTTACK_CREEPS
+            Memory.remoteData[room]?.threatLevel < RemoteRoomThreatLevel.ENEMY_ATTTACK_CREEPS
     );
 }
 
