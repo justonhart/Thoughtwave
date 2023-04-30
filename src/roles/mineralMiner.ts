9a6fc36bc806c474ff071412b2277120bfb40b30
Melvin Lipka <74310778+lipkam@users.noreply.github.com>
2023-04-30 09:49:28 +0200

    Recycle mineralMiner when finished

--- src/roles/mineralMiner.ts
+++ src/roles/mineralMiner.ts
@@ -7,7 +7,11 @@ export class MineralMiner extends WaveCreep {
             this.memory.currentTaskPriority = Priority.HIGH;
             let container = this.pos.lookFor(LOOK_STRUCTURES).find((s) => s.structureType === STRUCTURE_CONTAINER) as StructureContainer;
             if (container?.store.getFreeCapacity() >= this.getActiveBodyparts(WORK)) {
-                this.harvest(this.room.mineral);
+                const result = this.harvest(this.room.mineral);
+                if (result === ERR_NOT_ENOUGH_RESOURCES) {
+                    // Finished mining
+                    this.memory.recycle = true;
+                }
             }
         } else {
             this.travelTo(assignedPos, { maxOps: 20000, avoidHostileRooms: true });
