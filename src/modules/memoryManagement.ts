aa901ebb569c328e299e2cb05834b68d05582f89
Melvin Lipka <74310778+lipkam@users.noreply.github.com>
2023-05-05 11:25:12 +0200

    Remove dead rooms properly

--- src/modules/memoryManagement.ts
+++ src/modules/memoryManagement.ts
@@ -14,7 +14,7 @@ export function manageMemory() {
     // Cleanup dead rooms
     if (Game.time % 100 === 0) {
         Object.keys(Memory.rooms)
-            .filter((roomName) => !Object.keys(Memory.rooms[roomName]).length)
+            .filter((roomName) => !Game.rooms[roomName]) // Owned rooms always have vision
             .forEach((roomName) => delete Memory.rooms[roomName]);
     }
 
