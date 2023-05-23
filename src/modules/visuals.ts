4e8be3c63caa062dbdf7aa98bdda33c6968c7902
Juston Hart <juston@ou.edu>
2023-05-23 11:40:10 -0500

    Simplify debug road drawing

--- src/modules/visuals.ts
+++ src/modules/visuals.ts
@@ -3,9 +3,6 @@ import { drawLayout } from './roomDesign';
 
 export function runVisuals() {
     visualizeRoomData();
-    if (Memory.debug?.drawRoads) {
-        drawRoadsFromRoomData();
-    }
 
     if (Memory.debug?.drawRemoteConnections) {
         drawLinesToRemoteRooms();
@@ -16,30 +13,28 @@ export function runVisuals() {
     }
 }
 
-function drawRoadsFromRoomData() {
-    Object.keys(Memory.roomData).forEach((room) => {
-        let rv = new RoomVisual(room);
-        let roads = Memory.roomData[room]?.roads ? Object.values(Memory.roomData[room].roads) : [];
-        if (roads?.length) {
-            let roadSegments = [];
-            roads.forEach((roadCode) => {
-                let road = decodeRoad(roadCode, room);
-                getRoadSegments(road).forEach((segment) => roadSegments.push(segment));
-            });
-            roadSegments.forEach((roadSegment) => {
-                rv.poly(roadSegment, { lineStyle: 'dotted', stroke: '#63FF00', opacity: 100 });
-                Game.map.visual.poly(roadSegment, { lineStyle: 'dotted', stroke: '#63FF00', opacity: 100 });
-            });
-        }
-    });
-}
-
 function visualizeRoomData() {
     Object.entries(Memory.roomData).forEach(([roomName, data]) => {
         if (data.hostile) {
             Game.map.visual.rect(new RoomPosition(0, 0, roomName), 50, 50, { fill: '#8b0000', stroke: '#8b0000', strokeWidth: 2, opacity: 0.25 });
         } else if (data.asOf) {
             Game.map.visual.rect(new RoomPosition(0, 0, roomName), 50, 50, { opacity: 0.15, stroke: '#00ffff', strokeWidth: 2 });
+        }
+
+        if(Memory.debug.drawRoads){
+            let rv = new RoomVisual(roomName);
+            let roads = Memory.roomData[roomName]?.roads ? Object.values(Memory.roomData[roomName].roads) : [];
+            if (roads?.length) {
+                let roadSegments = [];
+                roads.forEach((roadCode) => {
+                    let road = decodeRoad(roadCode, roomName);
+                    getRoadSegments(road).forEach((segment) => roadSegments.push(segment));
+                });
+                roadSegments.forEach((roadSegment) => {
+                    rv.poly(roadSegment, { lineStyle: 'dotted', stroke: '#63FF00', opacity: 100 });
+                    Game.map.visual.poly(roadSegment, { lineStyle: 'dotted', stroke: '#63FF00', opacity: 100 });
+                });
+            }
         }
     });
 }
