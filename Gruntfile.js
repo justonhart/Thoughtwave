ee363ba1d00159b6feeae77a220ee450588d2f17
Juston Hart <contact@justonh.art>
2023-05-27 08:59:57 -0500

    Add season parameter to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,7 @@ module.exports = function (grunt) {
                 email: process.env.email,
                 token: process.env.token,
                 branch: 'default',
+                server: 'season'
             },
             dist: {
                 src: ['dist/*.js'],
