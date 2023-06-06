5957cb0144c617058f92ac371a8ccb72a0080c4f
Juston Hart <juston@ou.edu>
2023-06-06 14:25:33 -0500

    Add suspend clause to thorium transport if invader core

--- src/roles/thoriumTransporter.ts
+++ src/roles/thoriumTransporter.ts
@@ -9,6 +9,9 @@ export class ThoriumTransporter extends Operative{
                 this.withdraw(storage, RESOURCE_THORIUM);
             } else {
                 this.travelTo(storage, {range: 1});
+                if(this.room.find(FIND_HOSTILE_STRUCTURES, {filter: s => s.structureType === STRUCTURE_INVADER_CORE}).length) {
+                    Memory.operations[this.memory.operationId].stage = OperationStage.SUSPEND;
+                }
             }
         } else {
             const reactor = Game.getObjectById((this.operation as ThoriumOperation).reactor) as Reactor;
